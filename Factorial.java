56eed837b7c20355b2016229eadd904f30b9f805
Ondrej Krutsky <ondrej@krutsky.com>
2025-08-12 23:21:17 +0200

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+package math;
+import java.util.Scanner;
+
+public class Factorial {
+	
+	public int factorial(int n) {
+		
+		return(n== 1 || n == 0) ? 1 : n * factorial(n-1);
+		
+	}
+	
+	public static void main(String[] args) {
+
+		Factorial f = new Factorial(); // creating and initializing object
+		Scanner sc = new Scanner(System.in); 
+		System.out.println("Enter a number: ");
+		int i = sc.nextInt(); // reading input from a user
+		System.out.println("Factorial of " + i + "is " + f.factorial(i)); // calling a method and calculating		
+	}
+}
